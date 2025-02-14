2496a20508d19b189f977aa4d5328316d50e653f
SeolJaeHyeok <milkboy2564@naver.com>
2025-02-14 16:39:28 +0900

    sw force update

--- you_dont_need_refresh_browser/next-example/app/page.tsx
+++ you_dont_need_refresh_browser/next-example/app/page.tsx
@@ -6,15 +6,37 @@ import { useEffect } from "react";
 export default function Home() {
 
   useEffect(() => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((registration) =>
-        console.log(
-          "Service Worker registration successful with scope: ",
-          registration.scope,
-        ),
-      )
-      .catch((err) => console.log("Service Worker registration failed: ", err));
+    const registerServiceWorker = async () => {
+      if ('serviceWorker' in navigator) {
+        try {
+          const registration = await navigator.serviceWorker.register("/service-worker.js");
+          console.log("Service Worker registration successful with scope: ", registration.scope);
+
+          // 서비스 워커 업데이트 확인
+          registration.onupdatefound = () => {
+            const newWorker = registration.installing;
+            if (newWorker) {
+              newWorker.onstatechange = () => {
+                if (newWorker.state === 'installed') {
+                  if (navigator.serviceWorker.controller) {
+                    console.log("New content is available; please refresh.");
+                  } else {
+                    console.log("Content is cached for offline use.");
+                  }
+                }
+              };
+            }
+          };
+
+          // 강제로 업데이트 확인
+          await registration.update();
+        } catch (err) {
+          console.log("Service Worker registration failed: ", err);
+        }
+      }
+    };
+
+    registerServiceWorker();
   }, []);
 
   return (
